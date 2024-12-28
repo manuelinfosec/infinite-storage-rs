4f4ed7ff0e7ebf99713d0bb08b7d8e53f460b1f6
Manuel <manuelinfosec@gmail.com>
2024-12-29 00:20:55 +0100

    args: adding download and disloding cmd params"

--- src/args.rs
+++ src/args.rs
@@ -23,6 +23,14 @@ pub enum EmbedPreset {
     MaxEfficiency,
 }
 
+#[derive(Debug, Clone, ValueEnum)]
+pub enum EmbedOutputMode {
+    /// Uses RGB values and breaks under compression
+    Colored,
+    /// Uses black and white pixels and resists compression
+    Binary,
+}
+
 /// This encodes the specific params foro embedding
 /// All values are optional, and will be substituted in using UI if missing.
 #[derive(Args, Default, Debug)]
@@ -35,4 +43,48 @@ pub struct EmbedParams {
     /// More specific encoding options override preset options
     #[arg(short, long)]
     pub preset: Option<EmbedPreset>,
+
+    /// Etching mode
+    #[arg(long)]
+    pub mode: Option<EmbedOutputMode>,
+
+    /// Block size, in pixels per side
+    #[arg(long)]
+    pub block_size: Option<i32>,
+
+    /// Thread to use when encoding
+    #[arg(long)]
+    pub threads: Option<usize>,
+
+    /// Output video FPS
+    #[arg(long)]
+    pub fps: Option<i32>,
+
+    /// Output videoo resolution.
+    /// Must be one oof "144", "240", "360", "480" or "720",
+    /// and if the value provided is none of these, defaults
+    /// to "360"
+    #[arg(long)]
+    pub resolution: Option<String>,
+}
+
+/// This encodes the specific params for downloading.
+/// All values are optioal, and ill be substituted in using UI if missing
+#[derive(Args, Default)]
+pub struct DownloadParams {
+    #[arg(short, long)]
+    pub url: Option<String>,
+}
+
+/// This encodes the specific params for dislodginig.
+/// All values are optional, and will be substituted in using UI if missing
+#[derive(Args, Default)]
+pub struct DislodgeParams {
+    /// Path to input viideoo
+    #[arg(short, long)]
+    pub in_path: Option<String>,
+
+    /// Path to file output (including extension)
+    #[arg(short, long)]
+    pub out_path: Option<String>,
 }
